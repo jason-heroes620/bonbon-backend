6a4c2854aeb288a8a5254d38f1038691cf76c015
jason-heroes620 <jason.w@heroes.my>
2026-03-23 00:19:34 +0800

    check log in

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
+        Log::info('Login request', $request->all());
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
