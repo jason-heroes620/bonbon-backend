8f320ec3018e17678119f57a738df8661f3855fa
jason-heroes620 <jason.w@heroes.my>
2026-05-28 16:10:18 +0800

    update user membership column

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\CreditTransactions;
+use App\Models\Memberships;
 use App\Models\User;
 use App\Models\UserMemberships;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function show($user_id)
+    protected int $max_vouchers = 5;
+
+    public function show(string $user_id)
     {
         $user = User::find($user_id, 'user_id');
         $user->membership = $this->getUserMembership($user->user_id);
@@ -71,11 +74,66 @@ class UserController extends Controller
         ]);
     }
 
+    public function trial(Request $request)
+    {
+        $user = $request->user();
+        if (!$user) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        }
+
+        // update user membership to trial membership
+        try {
+            $membership = Memberships::query()
+                ->select(['membership_id', 'membership_type'])
+                ->where('membership_type', 'Trial')
+                ->first();
+
+            if (!$membership) {
+                return response()->json([
+                    'message' => 'Trial membership not found.',
+                ], 404);
+            }
+
+            UserMemberships::query()
+                ->where('user_id', $user->user_id)
+                ->where('membership_status', 'active')
+                ->update([
+                    'membership_status' => 'inactive',
+                    'membership_end_date' => now()->subDay(1)->toDateString(),
+                ]);
+
+            UserMemberships::create([
+                'user_id' => $user->user_id,
+                'membership_id' => $membership->membership_id,
+                'membership_status' => 'active',
+                'membership_start_date' => now()->toDateString(),
+            ]);
+
+            $membership->max_vouchers = $this->max_vouchers;
+
+            return response()->json([
+                'message' => 'User membership updated successfully.',
+                'membership' => $membership,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
     private function getUserMembership(string $user_id)
     {
-        $membership = UserMemberships::where('user_id', $user_id)
+        $membership = UserMemberships::query()
+            ->where('user_id', $user_id)
             ->join('memberships', 'memberships.membership_id', '=', 'user_memberships.membership_id')
-            ->select('memberships.membership_type', 'user_memberships.membership_status', 'user_memberships.membership_end_date')
+            ->select(
+                'memberships.membership_type',
+                'user_memberships.membership_status',
+                'user_memberships.membership_end_date',
+                'user_memberships.max_vouchers',
+                'user_memberships.redeemed_vouchers_count',
+            )
             ->where('user_memberships.membership_status', 'active')
             ->first();
 
